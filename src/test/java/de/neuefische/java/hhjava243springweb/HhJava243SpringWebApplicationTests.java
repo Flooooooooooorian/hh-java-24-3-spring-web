4ad68a670f626153e1b87792cf8f1ec877740f72
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-07-18 09:56:43 +0200

    Init Spring Boot Project

---
+++ src/test/java/de/neuefische/java/hhjava243springweb/HhJava243SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.java.hhjava243springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava243SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
